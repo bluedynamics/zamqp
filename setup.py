be0edd13cb4076c2045b2df75ad371a22f261dfb
Robert Niederreiter <rnix@squarewave.at>
2010-12-11 13:55:37 +0100

    add LICENCE.rst

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ import sys, os
 version = '1.0b2'
 shortdesc = 'AMQP broadcasting for python and zope.'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
 tests_require = ['interlude']
+
 setup(name='zamqp',
       version=version,
       description=shortdesc,
